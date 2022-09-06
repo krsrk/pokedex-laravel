3c9e7d13ad65b07e94ed2ecc58ac147bbcec1901
Chris Lopez <christian.lopez.be@gmail.com>
2022-09-06 15:41:29 -0500

    fix(): delete braces on footer

--- resources/js/components/PokedexFooter.vue
+++ resources/js/components/PokedexFooter.vue
@@ -5,7 +5,7 @@
             <div class="mt-16 border-t-2 border-gray-300 flex flex-col items-center">
                 <div class="sm:w-2/3 text-center py-6">
                     <p class="text-sm text-blue-700 font-bold mb-2">
-                        © {{ currentYear }} Pokedex Project by Chris López.}}
+                        © {{ currentYear }} Pokedex Project by Chris López.
                     </p>
                 </div>
             </div>
